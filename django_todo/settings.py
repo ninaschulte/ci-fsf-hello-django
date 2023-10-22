730d895ea2e4c0cf0c7b2fe5043a1c530049ff2c
ninaschulte <>
2023-10-22 13:43:33 +0000

    add Procfile

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
 import os
+if os.path.isfile("env.py"):
+    import env
+from pathlib import Path
 import dj_database_url
 if os.path.isfile('env.py'):
 
