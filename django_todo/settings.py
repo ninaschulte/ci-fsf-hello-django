3d1a73548e59392ee7ed6b651ce2d5b75f5ae5f8
ninaschulte <>
2023-10-23 17:38:53 +0000

    Add settings fix for allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -15,7 +15,9 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
-    # Build paths inside the project like this: BASE_DIR / 'subdir'.
+development = os.environ.get('DEVELOPMENT', False)
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,12 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(x*34%(u6zmh1m#jvx0lk%_0elp2@znyvb!b&^q_wy*&bqgpks')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-(x*34%(u6zmh1m#jvx0lk%_0elp2@znyvb!b&^q_wy*&bqgpks')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = ['nina-django-todo-app-7f7819ce3c7b.herokuapp.com']
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -77,17 +83,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
